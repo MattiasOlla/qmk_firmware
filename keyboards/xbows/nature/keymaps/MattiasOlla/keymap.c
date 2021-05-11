9a1abc00efb8367183c282ea6785445a9578b3b2
Mattias Olla <mattias.olla@gmail.com>
2021-05-11 22:44:10 +0200

    Add my own keymap
    Enable åäö using alt+numpad codes

---
+++ keyboards/xbows/nature/keymaps/MattiasOlla/keymap.c
@@ -0,0 +1,77 @@
+#include QMK_KEYBOARD_H
+
+#define MODS_SHIFT_MASK (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
+
+enum custom_keycodes {
+    SE_AA = SAFE_RANGE,
+    SE_AE,
+    SE_OE,
+};
+
+const char *swedish_codes[][2] = {
+    {
+        SS_RALT(SS_TAP(X_KP_1)SS_TAP(X_KP_3)SS_TAP(X_KP_4)), // Alt 134 -> å
+        SS_RALT(SS_TAP(X_KP_1)SS_TAP(X_KP_4)SS_TAP(X_KP_3)), // Alt 143 -> Å
+    },
+    {
+        SS_RALT(SS_TAP(X_KP_1)SS_TAP(X_KP_3)SS_TAP(X_KP_2)), // Alt 132 -> ä
+        SS_RALT(SS_TAP(X_KP_1)SS_TAP(X_KP_4)SS_TAP(X_KP_2)), // Alt 142 -> Ä
+    },
+    {
+        SS_RALT(SS_TAP(X_KP_1)SS_TAP(X_KP_4)SS_TAP(X_KP_8)), // Alt 148 -> ö
+        SS_RALT(SS_TAP(X_KP_1)SS_TAP(X_KP_5)SS_TAP(X_KP_3)), // Alt 153 -> Ö
+    },
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (!record->event.pressed) return true;
+  switch (keycode) {
+    case SE_AA: case SE_AE: case SE_OE: {
+      uint8_t mods = get_mods();
+      clear_mods();
+      // Send code based on which key was pressed and whether Shift was held.
+      uint16_t index = keycode - SE_AA;
+      uint8_t shift = mods & MODS_SHIFT_MASK;
+
+      send_string(swedish_codes[index][(bool)shift]);
+
+      set_mods(mods);
+      return false;
+    }
+    default:
+      return true;
+  }
+}
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  /* Keymap VANILLA: (Base Layer) Default Layer
+   *
+   * |---------------------------------------------------------------------------------------------------------------------------------|
+   * | Esc |  F1  |  F2  |  F3  |  F4  |      F5  |  F6  |  F7  |  F8  |      F9  |  F10 |  F11 |  F12 |   Delete   |    Prtsc    |
+   * |---------------------------------------------------------------------------------------------------------------------------------|
+   * |  ~  |     1   |   2   |   3   |   4   |    5      |       6    |    7    |    8   |   9  |   0  |   -  |  =  |  Backspace  |
+   * |---------------------------------------------------------------------------------------------------------------------------------|
+   * | Tab |   Q    |    W   |   E  |   R  |   T  |            |    Y   |    U   |    I  |   O  |   P  |   [  |  ]  |   \  | PgUp |
+   * |---------------------------------------------------------------------------------------------------------------------------------|
+   * | Ctl |   A   |   S   |   D  |   F  |   G  |      Bksp      |    H  |    J   |   K  |   L  |   ;  |  '"  |    Enter   | PgDn |
+   * |---------------------------------------------------------------------------------------------------------------------------------|
+   * |Shift|   Z  |   X  |   C  |   V  |   B  |       Enter       |    N  |    M   |  ,  |   .  |  /?  | Shift|      |  Up |
+   * |---------------------------------------------------------------------------------------------------------------------------------|
+   * |Ctrl | GUI |     Alter   |    Space   |   Ctrl   |   Shift   |     Space     |    Alter   |  FN  | Ctrl | Lft  |  Dn |  Rig |
+   * |---------------------------------------------------------------------------------------------------------------------------------|
+   */
+  [0] = LAYOUT(
+		KC_ESC,  KC_F1,   KC_F2, KC_F3,  KC_F4,  KC_F5,   KC_F6,   KC_F7, KC_F8,  KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_DEL,  KC_PSCR,
+		KC_GRV,  KC_1,    KC_2,  KC_3,   KC_4,   KC_5,             KC_6,   KC_7,  KC_8,    KC_9,     KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+		KC_TAB,  KC_Q,    KC_W,  KC_E,   KC_R,   KC_T,    KC_Y,    KC_U,  KC_I,   KC_O,    KC_P,     KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
+		KC_CAPS, KC_A,    KC_S,  KC_D,   KC_F,   KC_G,    KC_BSPC, KC_H,  KC_J,   KC_K,    KC_L,     KC_SCLN, KC_QUOT, KC_ENT,  KC_PGDN,
+		KC_LSFT, KC_Z,    KC_X,  KC_C,   KC_V,   KC_B,    KC_ENT,  KC_N,  KC_M,   KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT, KC_UP,
+		KC_LCTL, MO(1),   KC_LALT,       KC_SPC, KC_LCTL, KC_LSFT, KC_SPC,        KC_RALT, KC_LGUI,  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
+  [1] = LAYOUT(
+    RESET,     KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_CALC,   KC_MYCM,  KC_MSEL,    KC_MAIL,    NK_TOGG,   EEP_RST,
+    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,             KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_NLCK,
+    RGB_TOG,   RGB_MOD, RGB_VAI, RGB_HUI,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  SE_AA,      KC_TRNS,    KC_TRNS,   KC_MNXT,
+    KC_TRNS,   RGB_SPD, RGB_VAD, RGB_SPI,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  SE_OE,      SE_AE,      KC_TRNS,   KC_MPRV,
+    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,    KC_MUTE,    KC_VOLU,
+    KC_TRNS,   KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,            KC_TRNS,   KC_TRNS,  KC_MPLY,    KC_HOME,    KC_VOLD,   KC_END)
+};
