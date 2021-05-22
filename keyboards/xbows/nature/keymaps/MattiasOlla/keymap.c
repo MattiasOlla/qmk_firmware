e34b3aee80e45cbffc90c30ce78727dc118a82b3
Mattias Olla <mattias.olla@gmail.com>
2021-05-22 19:44:48 +0200

    Add comments to shield keymap from autoformat

--- keyboards/xbows/nature/keymaps/MattiasOlla/keymap.c
+++ keyboards/xbows/nature/keymaps/MattiasOlla/keymap.c
@@ -1,6 +1,6 @@
 #include QMK_KEYBOARD_H
 
-#define MODS_SHIFT_MASK (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
+#define MODS_SHIFT_MASK (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT))
 
 enum custom_keycodes {
     SE_AA = SAFE_RANGE,
@@ -10,30 +10,32 @@ enum custom_keycodes {
 
 const char *swedish_codes[][2] = {
     {
-        SS_RALT(SS_TAP(X_KP_1)SS_TAP(X_KP_3)SS_TAP(X_KP_4)), // Alt 134 -> å
-        SS_RALT(SS_TAP(X_KP_1)SS_TAP(X_KP_4)SS_TAP(X_KP_3)), // Alt 143 -> Å
+        SS_RALT(SS_TAP(X_KP_1) SS_TAP(X_KP_3) SS_TAP(X_KP_4)),  // Alt 134 -> å
+        SS_RALT(SS_TAP(X_KP_1) SS_TAP(X_KP_4) SS_TAP(X_KP_3)),  // Alt 143 -> Å
     },
     {
-        SS_RALT(SS_TAP(X_KP_1)SS_TAP(X_KP_3)SS_TAP(X_KP_2)), // Alt 132 -> ä
-        SS_RALT(SS_TAP(X_KP_1)SS_TAP(X_KP_4)SS_TAP(X_KP_2)), // Alt 142 -> Ä
+        SS_RALT(SS_TAP(X_KP_1) SS_TAP(X_KP_3) SS_TAP(X_KP_2)),  // Alt 132 -> ä
+        SS_RALT(SS_TAP(X_KP_1) SS_TAP(X_KP_4) SS_TAP(X_KP_2)),  // Alt 142 -> Ä
     },
     {
-        SS_RALT(SS_TAP(X_KP_1)SS_TAP(X_KP_4)SS_TAP(X_KP_8)), // Alt 148 -> ö
-        SS_RALT(SS_TAP(X_KP_1)SS_TAP(X_KP_5)SS_TAP(X_KP_3)), // Alt 153 -> Ö
+        SS_RALT(SS_TAP(X_KP_1) SS_TAP(X_KP_4) SS_TAP(X_KP_8)),  // Alt 148 -> ö
+        SS_RALT(SS_TAP(X_KP_1) SS_TAP(X_KP_5) SS_TAP(X_KP_3)),  // Alt 153 -> Ö
     },
 };
 
 uint8_t mods;
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+bool    process_record_user(uint16_t keycode, keyrecord_t *record) {
     mods = get_mods();
     switch (keycode) {
-        case SE_AA: case SE_AE: case SE_OE: {
+        case SE_AA:
+        case SE_AE:
+        case SE_OE: {
             if (!record->event.pressed) return true;
 
             clear_mods();
             // Send code based on which key was pressed and whether Shift was held.
             uint16_t index = keycode - SE_AA;
-            uint8_t shift = mods & MODS_SHIFT_MASK;
+            uint8_t  shift = mods & MODS_SHIFT_MASK;
 
             send_string(swedish_codes[index][(bool)shift]);
 
@@ -58,7 +60,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     set_mods(mods);
                     return false;
                 }
-            } else { // on release of KC_BSPC
+            } else {  // on release of KC_BSPC
                 // In case KC_DEL is still being sent even after the release of KC_BSPC
                 if (delkey_registered) {
                     unregister_code(KC_DEL);
@@ -75,22 +77,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Keymap VANILLA: (Base Layer) Default Layer
-   *
-   * |---------------------------------------------------------------------------------------------------------------------------------|
-   * | Esc |  F1  |  F2  |  F3  |  F4  |      F5  |  F6  |  F7  |  F8  |      F9  |  F10 |  F11 |  F12 |   Delete   |    Prtsc    |
-   * |---------------------------------------------------------------------------------------------------------------------------------|
-   * |  ~  |     1   |   2   |   3   |   4   |    5      |       6    |    7    |    8   |   9  |   0  |   -  |  =  |  Backspace  |
-   * |---------------------------------------------------------------------------------------------------------------------------------|
-   * | Tab |   Q    |    W   |   E  |   R  |   T  |            |    Y   |    U   |    I  |   O  |   P  |   [  |  ]  |   \  | PgUp |
-   * |---------------------------------------------------------------------------------------------------------------------------------|
-   * | Ctl |   A   |   S   |   D  |   F  |   G  |      Bksp      |    H  |    J   |   K  |   L  |   ;  |  '"  |    Enter   | PgDn |
-   * |---------------------------------------------------------------------------------------------------------------------------------|
-   * |Shift|   Z  |   X  |   C  |   V  |   B  |       Enter       |    N  |    M   |  ,  |   .  |  /?  | Shift|      |  Up |
-   * |---------------------------------------------------------------------------------------------------------------------------------|
-   * |Ctrl | GUI |     Alter   |    Space   |   Ctrl   |   Shift   |     Space     |    Alter   |  FN  | Ctrl | Lft  |  Dn |  Rig |
-   * |---------------------------------------------------------------------------------------------------------------------------------|
-   */
+    /* Keymap VANILLA: (Base Layer) Default Layer
+     *
+     * |---------------------------------------------------------------------------------------------------------------------------------|
+     * | Esc |  F1  |  F2  |  F3  |  F4  |      F5  |  F6  |  F7  |  F8  |      F9  |  F10 |  F11 |  F12 |   Delete   |    Prtsc    |
+     * |---------------------------------------------------------------------------------------------------------------------------------|
+     * |  ~  |     1   |   2   |   3   |   4   |    5      |       6    |    7    |    8   |   9  |   0  |   -  |  =  |  Backspace  |
+     * |---------------------------------------------------------------------------------------------------------------------------------|
+     * | Tab |   Q    |    W   |   E  |   R  |   T  |            |    Y   |    U   |    I  |   O  |   P  |   [  |  ]  |   \  | PgUp |
+     * |---------------------------------------------------------------------------------------------------------------------------------|
+     * | Ctl |   A   |   S   |   D  |   F  |   G  |      Bksp      |    H  |    J   |   K  |   L  |   ;  |  '"  |    Enter   | PgDn |
+     * |---------------------------------------------------------------------------------------------------------------------------------|
+     * |Shift|   Z  |   X  |   C  |   V  |   B  |       Enter       |    N  |    M   |  ,  |   .  |  /?  | Shift|      |  Up |
+     * |---------------------------------------------------------------------------------------------------------------------------------|
+     * |Ctrl | GUI |     Alter   |    Space   |   Ctrl   |   Shift   |     Space     |    Alter   |  FN  | Ctrl | Lft  |  Dn |  Rig |
+     * |---------------------------------------------------------------------------------------------------------------------------------|
+     */
+    // clang-format off
     [0] = LAYOUT(
         KC_ESC,        KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           KC_PSCR,
         KC_GRV,        KC_1,    KC_2,    KC_3,     KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
@@ -107,4 +110,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LSFT(KC_LGUI), KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE,          KC_VOLU,
         KC_TRNS,       KC_TRNS, KC_TRNS,           KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,          KC_TRNS, KC_MPLY, KC_HOME, KC_VOLD, KC_END
     )
+    // clang-format on
 };
